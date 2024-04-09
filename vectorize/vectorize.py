24b5f836d8c3048c062768714eab7768f4e07899
jbassinot <bassinot.julien@gmail.com>
2024-04-09 13:36:04 +0200

    Refactor vectorize.py

--- vectorize/vectorize.py
+++ vectorize/vectorize.py
@@ -1,13 +1,13 @@
 import geopandas as gpd
 import rioxarray as riox
 from shapely.geometry import box, mapping, Polygon, MultiPolygon
+from collections import defaultdict
 import numpy as np
 import cv2
 import argparse
 import logging
 
 
-# [longitude, latitude] -> [longitude, latitude]
 def epsg_to_pixel(x, raster):
     mat = raster.rio.transform()
     u = np.clip(np.round((x[0] - mat[2]) / mat[0]), 0, raster.shape[1]).astype(int)
@@ -15,77 +15,92 @@ def epsg_to_pixel(x, raster):
     return [u, v]
 
 
-def pixel_to_epsg(x, raster):
-    mat = raster.rio.transform()
-    return x * np.array([mat[0], mat[4]]) + np.array([mat[2], mat[5]])
+def mask_to_polygons(mask, min_area=10.):
+    """Convert a mask ndarray (binarized image) to Multipolygons"""
+    # first, find contours with cv2: it's much faster than shapely
+    contours, hierarchy = cv2.findContours(mask, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_NONE)
+    if not contours:
+        return []
+    cnt_children = defaultdict(list)
+    child_contours = set()
+    assert hierarchy.shape[0] == 1
+    for idx, (_, _, _, parent_idx) in enumerate(hierarchy[0]):
+        if parent_idx != -1:
+            child_contours.add(idx)
+            cnt_children[parent_idx].append(contours[idx])
+    # create actual polygons filtering by area (removes artifacts)
+    all_polygons = []
+    for idx, cnt in enumerate(contours):
+        if idx not in child_contours and cv2.contourArea(cnt) >= min_area:
+            assert cnt.shape[1] == 1
+            poly = Polygon(
+                shell=cnt[:, 0, :],
+                holes=[c[:, 0, :] for c in cnt_children.get(idx, [])
+                       if cv2.contourArea(c) >= min_area])
+            all_polygons.append(poly)
+    return all_polygons
 
 
-def get_lake(geometry, idx, date, tuile, raster, whole_lake):
-    # for each lake crop a ROI in raster
-    frame = list(geometry.geoms)[0]
-    # left, bottom, right, top
-    frame = frame.bounds
-    # [[left, top], [right, bottom]]
-    frame = [[frame[0] - 50, frame[3] + 50],
-             [frame[2] + 50, frame[1] - 50]]
-    frame = [epsg_to_pixel(x, raster) for x in frame]
-    # Check if there's a lake detected in raster
-    if whole_lake[frame[0][1] + (frame[1][1] - frame[0][1]) // 2, frame[0][0] + (frame[1][0] - frame[0][0]) // 2] == 0:
+def resolve_geometries(contours, min_area):
+    new_contours = []
+    for cnt in contours:
+        if cnt.is_valid:
+            new_contours.append(cnt)
+        else:
+            cnt = cnt.buffer(0)
+            if cnt.geom_type == 'Polygon':
+                new_contours.append(cnt)
+            else:
+                for c in cnt.geoms:
+                    if c.area > min_area:
+                        new_contours.append(c)
+    return new_contours
+
+
+def get_lake(geometry, idx, date, tuile, raster):
+    frame = geometry.bounds
+    buffer = np.max((50, 0.2 * np.sqrt(geometry.area)))
+    frame = [[frame[0] - buffer, frame[3] + buffer],
+             [frame[2] + buffer, frame[1] - buffer]]
+    frame_img = [epsg_to_pixel(x, raster) for x in frame]
+    vignette_mask = raster[frame_img[0][1]: frame_img[1][1], frame_img[0][0]: frame_img[1][0]]
+    inpe_poly = []
+    for j in range(len(geometry.geoms)):
+        coord = [[x, y] for x, y, z in geometry.geoms[j].exterior.coords]
+        coord = [epsg_to_pixel(x, raster) for x in coord]
+        coord = [(x[0]-frame_img[0][0], x[1]-frame_img[0][1]) for x in coord]
+        inpe_poly.append(Polygon(coord).buffer(0))
+    contours = mask_to_polygons(vignette_mask.values.astype(np.uint8), 10)
+    contours = resolve_geometries(contours, 10)
+    inpe_poly = resolve_geometries(inpe_poly, 10)
+    if len(contours) == 0:
         return
-    lake = whole_lake[frame[0][1]: frame[1][1], frame[0][0]: frame[1][0]]
-
-    # retrieve lake contour from binary raster
-    contours, hierarchy = cv2.findContours(lake, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)  # cv2.CHAIN_APPROX_NONE
-
-    # in case of several contours choose the one at the center
-    dst = 999999
-    contour = None
-    for c in contours:
-        M = cv2.moments(c)
-        if M["m00"] == 0:
-            continue
-        cX = int(M["m10"] / M["m00"])
-        cY = int(M["m01"] / M["m00"])
-        dst_c = np.sqrt((cX - lake.shape[1] // 2) ** 2 + (cY - lake.shape[0] // 2) ** 2)
-        if dst_c < dst:
-            contour = c
-            dst = dst_c
-    if contour is None or dst > 5:
+    list_polygon = []
+    for contour in contours:
+        if np.any([contour.intersects(inpe_poly[j]) for j in range(len(inpe_poly))]):
+            list_polygon.append(contour)
+    if len(list_polygon) != 0:
+        return ({"id_inpe": idx,
+                 "date": date,
+                 "tuile": tuile,
+                 "geometry": MultiPolygon(list_polygon)
+                 })
+    else:
         return
-
-    # convert contour into a polygon
-    contour = contour.squeeze()
-    contour = contour + np.array([frame[0][0], frame[0][1]])
-    contour = pixel_to_epsg(contour, raster)
-    contour = Polygon(contour)
-
-    # add contour to result list
-    return ({"id_inpe": idx,
-             "date": date,
-             "tuile": tuile,
-             "geometry": contour
-             })
 
 
 def vectorize(inpe, src, dst, date, tuile):
     data = gpd.read_file(inpe)
-    img = riox.open_rasterio(src).squeeze()
+    img = riox.open_rasterio(src).squeeze().rio.write_nodata(0, inplace=True)
 
-    # CRS Lambert 93
-    img = img.rio.write_nodata(0, inplace=True)
-    img = img.rio.reproject(data.crs, nodata=0)
-    img = img.astype('bool')
-    img = img.astype('uint8')
+    # convert inpe to tile crs
+    lambert_93_crs = data.crs
+    data.to_crs(img.rio.crs, inplace=True)
 
-    # Select shapes inside raster
-    data = data[data.geometry.intersects(box(*img.rio.bounds()))]
-
-    # prepare a list for storing the results
-    whole_lake = img.values
-
-    result = data.apply(lambda row: get_lake(row.geometry, row.id, date, tuile, img, whole_lake), axis=1)
+    result = data.apply(lambda row: get_lake(row.geometry, row.id, date, tuile, img), axis=1)
     result.dropna(inplace=True)
     result = gpd.GeoDataFrame(list(result.values), crs=data.crs)
+    result.to_crs(lambert_93_crs, inplace=True)
     result.to_file(dst)
 
     return
